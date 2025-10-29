e32f07961b5fc6e6b08a521844840d173bdb69b7
Ximtecs <michael.haahr101293@gmail.com>
2025-10-29 08:53:23 +0100

    [DEV] changed P2M->P2M_sphe - we may add other P2M version in the future.

--- pyFMM/moment/P2M_all.py
+++ pyFMM/moment/P2M_all.py
@@ -9,7 +9,23 @@ except Exception:
     import utils as utils  # type: ignore
 
 
-def P2M(X, q, p=4):
+def P2M_sphe(X, q, p=4):
+    """
+    Compute multipole expansion from point sources given in spherical coords.
+    Uses spherical harmonics to compute the multipole moments.
+    input:
+        X : (N, 3) ndarray
+            Spherical coordinates of source points (r, theta, phi).
+            where theta is the polar angle (0 <= theta <= pi)
+            and   phi   is the azimuthal angle (0 <= phi < 2pi
+        q : (N,) ndarray
+            Source strengths at points X.
+        p : int
+            Order of the multipole expansion.
+    output:
+        M : ((p+1)^2,) ndarray
+            Multipole moments flattened with index n^2 + n + m order.
+    """
     #------------- view of input array ----------------
     r = X[..., 0]; theta = X[..., 1]; phi = X[..., 2] 
     #--------------------------------------------------
