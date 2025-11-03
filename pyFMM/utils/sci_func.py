da5ab2628cda2d7e68990c1958c5a0d0016c73ee
Ximtecs <michael.haahr101293@gmail.com>
2025-11-03 13:25:43 +0100

    [DEV] added derivative functions required for dipole multipole expansion to sci_func.py

--- pyFMM/utils/sci_func.py
+++ pyFMM/utils/sci_func.py
@@ -11,6 +11,45 @@ def sph_harm_dir(n, m, theta, phi):
     return sqrt_fac * Pnm * np.exp(1j * m * phi)
 
 
+def dx_P(n, m, x):
+    if n == 0:
+        return 0.0
+    else:
+        #TODO - need some  check if x**2-1 is zero
+        return 1.0 / (x**2 - 1.0) * ( - ( n+ 1) * x * lpmv(m, n, x) + (n - m + 1) * lpmv(m, n + 1, x) )
+
+
+
+
+def dY_dtheta(n, m, theta, phi):
+    """
+    Compute the theta-derivative of the spherical harmonic Y_n^m(theta, phi).
+    inputs:
+        n, m: degree and order of the spherical harmonic.
+        theta, phi: arrays of polar and azimuthal angles.
+    outputs:
+        dY_dtheta: array of the theta-derivative of Y_n^m at the input angles.
+    """
+
+    sqrt_fac = np.sqrt( fac(n - abs(m)) / fac(n + abs(m)) )
+    dP = dx_P(n, abs(m), np.cos(theta))
+    res = - sqrt_fac * dP * np.sin(theta) * np.exp(1j * m * phi)
+    return res
+
+def dY_dphi(n, m, theta, phi):
+    """
+    Compute the phi-derivative of the spherical harmonic Y_n^m(theta, phi).
+    inputs:
+        n, m: degree and order of the spherical harmonic.
+        theta, phi: arrays of polar and azimuthal angles.
+    outputs:
+        dY_dphi: array of the phi-derivative of Y_n^m at the input angles.
+    """
+    Y = sph_harm_dir(n, m, theta, phi)
+    return 1j * m * Y
+
+
+
 def A_nm(n, m):
     """
     A_n^m = (-1)^n / sqrt((n-m)! (n+m)!)
